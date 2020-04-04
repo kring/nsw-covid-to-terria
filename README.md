be261af550429724af149a67936fbe833f3be53d
Kevin Ring <kevin@kotachrome.com>
2020-04-04 21:45:32 +1100

    Initial version.

---
+++ README.md
@@ -0,0 +1,11 @@
+A small program to process `covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area.csv` obtained from:
+https://data.nsw.gov.au/data/dataset/covid-19-cases-by-location/resource/21304414-1ff1-4243-a5d2-f52778048b29
+
+And produce a new CSV suitable for use in [NationalMap](https://nationalmap.gov.au) or [TerriaJS](https://terria.io).
+
+```
+npm install
+node index.js
+```
+
+Writes `covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area-for-terria.csv`.
---
+++ index.js
@@ -0,0 +1,56 @@
+const papaparse = require("papaparse");
+const fs = require("fs");
+const uniq = require("lodash/uniq");
+const sortedUniq = require("lodash/sortedUniq");
+const moment = require("moment");
+
+const csv = fs.readFileSync("covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area.csv", "utf8");
+const parsed = papaparse.parse(csv, {
+  header: true
+});
+
+const postcodes = uniq(parsed.data.map(row => row.postcode).filter(postcode => postcode && postcode.trim().length > 0 && postcode.trim() !== "0"));
+parsed.data.forEach(row => {
+  row.jsdate = Date.parse(row.notification_date);
+  row.moment = moment(row.jsdate);
+});
+
+const dates = sortedUniq(parsed.data.map(row => row.jsdate).filter(date => !Number.isNaN(date)).sort());
+
+const last3Days = [];
+const last7Days = [];
+const total = [];
+
+let result = "date,postcode,New Cases in Last 3 Days,New Cases in Last 7 Days,Total Cases\n";
+
+for (let i = 0; i < postcodes.length; ++i) {
+  last3Days[postcodes[i]] = [];
+  last7Days[postcodes[i]] = [];
+  total[postcodes[i]] = [];
+}
+
+for (let i = 0; i < dates.length; ++i) {
+  const thisDate = dates[i];
+  const thisMoment = moment(thisDate);
+
+  const onThisDate = parsed.data.filter(row => Date.parse(row.notification_date) === thisDate);
+
+  for (let j = 0; j < postcodes.length; ++j) {
+    const thisPostcode = postcodes[j];
+    const inThisPostcode = onThisDate.filter(row => row.postcode === thisPostcode);
+
+    total[thisPostcode].push(...inThisPostcode);
+
+    const lastThreeDays = thisMoment.clone().subtract(2, "days");
+    last3Days[thisPostcode] = last3Days[thisPostcode].filter(row => !row.moment.isBefore(lastThreeDays));
+    last3Days[thisPostcode].push(...inThisPostcode);
+
+    const lastSevenDays = thisMoment.clone().subtract(6, "days");
+    last7Days[thisPostcode] = last7Days[thisPostcode].filter(row => !row.moment.isBefore(lastSevenDays));
+    last7Days[thisPostcode].push(...inThisPostcode);
+
+    result += `${thisMoment.toISOString(true)},${thisPostcode},${last3Days[thisPostcode].length},${last7Days[thisPostcode].length},${total[thisPostcode].length}\n`;
+  }
+}
+
+fs.writeFileSync("covid-19-cases-by-notification-date-and-postcode-local-health-district-and-local-government-area-for-terria.csv", result, "utf8");
